35ebb7d3b3dc3b9af4f40acf844c462f6a5f30ac
chtmorris <agoosester@gmail.com>
2016-06-04 09:34:05 +0800

    Added dismiss News VC

---
+++ Ether Tracker/ETNewsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ETNewsViewController.swift
+//  Ether Tracker
+//
+//  Created by Charlie Morris on 04/06/2016.
+//  Copyright © 2016 Mind Fund Studio. All rights reserved.
+//
+
+import UIKit
+
+class ETNewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func closeXButtonTapped(sender: UIButton) {
+        
+        navigationController?.popViewControllerAnimated(true)
+    }
+
+}

6e2e6be2a5bbef0ffe33d6775c31e2e29dd60c85
chtmorris <agoosester@gmail.com>
2016-06-04 23:48:20 +0800

    Added collection view to newsVC

--- Ether Tracker/ETNewsViewController.swift
+++ Ether Tracker/ETNewsViewController.swift
@@ -8,19 +8,56 @@
 
 import UIKit
 
-class ETNewsViewController: UIViewController {
+class ETNewsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
+    // ==================
+    // MARK: - PROPERTIES
+    // ==================
+    
+    @IBOutlet weak var newsCollectionView: UICollectionView!
+    
+    
+    // =================
+    // MARK: - LIFECYCLE
+    // =================
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        newsCollectionView.delegate = self
+        newsCollectionView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // ======================
+    // MARK: - COLLECTIONVIEW
+    // ======================
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
     }
     
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("NewsCell", forIndexPath: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 115)
+    }
+    
+    
+    // ====================
+    // MARK: - INTERACTIONS
+    // ====================
 
     @IBAction func closeXButtonTapped(sender: UIButton) {
         navigationController?.popViewControllerAnimated(true)

47c544b5d903f89adc31b7fd48e1f181b1e87c7c
chtmorris <agoosester@gmail.com>
2016-06-06 20:14:24 +0800

    Added ETWebsiteVC

---
+++ Ether Tracker/ETWebsiteViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ETWebsiteViewController.swift
+//  Ether Tracker
+//
+//  Created by Charlie Morris on 06/06/2016.
+//  Copyright © 2016 Mind Fund Studio. All rights reserved.
+//
+
+import UIKit
+
+class ETWebsiteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

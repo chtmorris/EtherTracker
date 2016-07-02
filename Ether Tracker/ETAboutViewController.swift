93a0497eca4da552afcaf485ba2a1755e4fa1786
chtmorris <agoosester@gmail.com>
2016-07-02 08:02:11 +0800

    Added about VC

---
+++ Ether Tracker/ETAboutViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ETAboutViewController.swift
+//  Ether Tracker
+//
+//  Created by Charlie Morris on 01/07/2016.
+//  Copyright © 2016 Mind Fund Studio. All rights reserved.
+//
+
+import UIKit
+
+class ETAboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+    @IBAction func closeButtonTapped(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
